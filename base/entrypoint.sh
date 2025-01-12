4d39ba3a867d64ab9c9682b41afe224db9a1e1cb
ayoube94 <ayoubelhassouni1994@gmail.com>
2025-01-12 18:16:20 +0000

    Cambios grandes

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -3,6 +3,7 @@
 # Set some sensible defaults
 export CORE_CONF_fs_defaultFS=${CORE_CONF_fs_defaultFS:-hdfs://`hostname -f`:8020}
 
+# Función para añadir propiedades a los archivos XML
 function addProperty() {
   local path=$1
   local name=$2
@@ -13,6 +14,7 @@ function addProperty() {
   sed -i "/<\/configuration>/ s/.*/${escapedEntry}\n&/" $path
 }
 
+# Función para configurar archivos XML según las variables de entorno
 function configure() {
     local path=$1
     local module=$2
@@ -31,6 +33,7 @@ function configure() {
     done
 }
 
+# Configurar los archivos de configuración de Hadoop
 configure /etc/hadoop/core-site.xml core CORE_CONF
 configure /etc/hadoop/hdfs-site.xml hdfs HDFS_CONF
 configure /etc/hadoop/yarn-site.xml yarn YARN_CONF
@@ -38,10 +41,11 @@ configure /etc/hadoop/httpfs-site.xml httpfs HTTPFS_CONF
 configure /etc/hadoop/kms-site.xml kms KMS_CONF
 configure /etc/hadoop/mapred-site.xml mapred MAPRED_CONF
 
+# Configurar red multihomed si está activada
 if [ "$MULTIHOMED_NETWORK" = "1" ]; then
     echo "Configuring for multihomed network"
 
-    # HDFS
+    # Configuración de HDFS
     addProperty /etc/hadoop/hdfs-site.xml dfs.namenode.rpc-bind-host 0.0.0.0
     addProperty /etc/hadoop/hdfs-site.xml dfs.namenode.servicerpc-bind-host 0.0.0.0
     addProperty /etc/hadoop/hdfs-site.xml dfs.namenode.http-bind-host 0.0.0.0
@@ -49,15 +53,16 @@ if [ "$MULTIHOMED_NETWORK" = "1" ]; then
     addProperty /etc/hadoop/hdfs-site.xml dfs.client.use.datanode.hostname true
     addProperty /etc/hadoop/hdfs-site.xml dfs.datanode.use.datanode.hostname true
 
-    # YARN
+    # Configuración de YARN
     addProperty /etc/hadoop/yarn-site.xml yarn.resourcemanager.bind-host 0.0.0.0
     addProperty /etc/hadoop/yarn-site.xml yarn.nodemanager.bind-host 0.0.0.0
     addProperty /etc/hadoop/yarn-site.xml yarn.timeline-service.bind-host 0.0.0.0
 
-    # MAPRED
+    # Configuración de MAPRED
     addProperty /etc/hadoop/mapred-site.xml yarn.nodemanager.bind-host 0.0.0.0
 fi
 
+# Configuración de Ganglia para monitoreo si está configurado el host
 if [ -n "$GANGLIA_HOST" ]; then
     mv /etc/hadoop/hadoop-metrics.properties /etc/hadoop/hadoop-metrics.properties.orig
     mv /etc/hadoop/hadoop-metrics2.properties /etc/hadoop/hadoop-metrics2.properties.orig
@@ -78,6 +83,7 @@ if [ -n "$GANGLIA_HOST" ]; then
     done > /etc/hadoop/hadoop-metrics2.properties
 fi
 
+# Función para esperar que un servicio esté disponible
 function wait_for_it()
 {
     local serviceport=$1
@@ -108,9 +114,27 @@ function wait_for_it()
     echo "[$i/$max_try] $service:${port} is available."
 }
 
+# Esperar por los servicios previos si están configurados
 for i in ${SERVICE_PRECONDITION[@]}
 do
     wait_for_it ${i}
 done
 
+# Iniciar Hadoop
+echo "Starting Hadoop services..."
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+
+# Iniciar Jupyter Notebook
+echo "Starting Jupyter Notebook..."
+jupyter notebook --NotebookApp.allow_origin='*' \
+                 --NotebookApp.token='' \
+                 --NotebookApp.password='' \
+                 --NotebookApp.allow_remote_access=True \
+                 --notebook-dir=/media/notebooks \
+                 --ip=0.0.0.0 \
+                 --port=8889 \
+                 --no-browser
+
+# Ejecutar el comando principal
 exec $@
